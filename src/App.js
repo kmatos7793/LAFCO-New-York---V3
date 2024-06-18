3e5bf24997800c0830815c8c3e108293ab7269d2
kmatos7793 <88529154+kmatos7793@users.noreply.github.com>
2024-06-18 08:07:53 +0800

    updated product ID

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
   let InstanceId_ReviewsTab = "";
 
   //Product ID
-  let ProductId = "HH61";
+  let ProductId = "HH18W";
 
   //Clipboard text here.
   let ClipBoardHeading = "Follow these steps to add this";
